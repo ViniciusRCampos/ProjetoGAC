81612598d04503f5969689b481fa2c5ae1c163f9
Vinicius <vinicius_uo@hotmail.com>
2025-01-17 08:50:23 -0300

    :sparkles: Added process to schedule to run every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\ProcessPendingOperations;
+use App\Models\OperationLog;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('operations:dispatch-pending')->everyMinute(); // Ajuste conforme necessário
     }
 
     /**
